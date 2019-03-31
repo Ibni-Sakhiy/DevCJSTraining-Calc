c995e5fe8dfed14ce177fe2214e99dce1cb5297d
ibni-sakhiy <herpananda10@gmail.com>
2019-03-31 14:00:55 +0700

    DecimalRounding

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var input = document.getElementById("display");
 function addNumber(character){
-    if(input.value==0 || input.value.lenght==0){
+    if(input.value==0 || input.value.length==0){
         input.value= character;
     }
     else{
@@ -8,7 +8,25 @@ function addNumber(character){
     }
 }
 function addResult (){
-    input.value= eval(input.value);
+    // input.value= eval(input.value);
+    if(input.value==0 || input.value.length==0){
+        input.value= 0;
+    } else {
+        var result = eval(input.value).toFixed(2);
+        for (i = 0; i < result.length;i++){
+            if(result[i]=='.'){
+                if(result[i+2]==0){
+                    if(result[i+1]==0){
+                        input.value = result.slice(0,-3);
+                    } else {
+                        input.value = result.slice(0,-1);
+                    }
+                } else {
+                    input.value = result;
+                }
+            }
+        }
+    }
 }
 function hilang() {
     input.value = 0;
